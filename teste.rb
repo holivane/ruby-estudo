f46e2a11907ceb74dd9c91106a6fea1e3686eedf
Holivane <holivane@gmail.com>
2023-08-26 13:33:44 -0300

    Teste com Ruby

---
+++ teste.rb
@@ -0,0 +1,24 @@
+idade = 27
+# ano = 2023
+
+=begin
+Bloco de código comentado
+rescue => exception
+=end
+
+idade = 34
+ano =2022
+
+nome = 'Lucas Lopes'
+
+
+if idade > 18
+  puts nome
+end
+
+puts idade if idade > 18
+
+caixa = nil
+caixa = 2
+
+puts "Caixa cheia" if caixa
